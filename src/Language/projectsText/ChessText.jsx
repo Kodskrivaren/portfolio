8c8a451bd9d362f421996fc6a093417a7dc7ba16
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2023-12-17 20:20:45 +0100

    removed br tags

--- src/Language/projectsText/ChessText.jsx
+++ src/Language/projectsText/ChessText.jsx
@@ -10,8 +10,6 @@ export default {
       src={"/images/projects/chess/chess3.jpg"}
       alt="basement"
     />,
-    <br key="chessBr1" />,
-    <br key="chessBr2" />,
     "Online servern skrev jag i JavaScript och körde lokalt i Node.js. När jag var nöjd med skriptet så skapade jag en server via Microsoft Azure som kör skriptet. Detta gjorde online delen fullt fungerande!",
   ],
   english: [
@@ -25,8 +23,6 @@ export default {
       src={"/images/projects/chess/chess3.jpg"}
       alt="basement"
     />,
-    <br key="chessBr1" />,
-    <br key="chessBr2" />,
     "I wrote the online server in JavaScript ran it locally in Node.js. When I was happy with the script I created a server through Microsoft Azure that runs the script. This made the online part fully functional!",
   ],
 };
