87d90c2f2563eb80066efaef52ade3d0a0e093a6
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-05-08 09:03:18 +0200

    uppdaterade om mig texten

--- src/Language/AboutMeText.jsx
+++ src/Language/AboutMeText.jsx
@@ -2,9 +2,15 @@ const Text = {
   mainText: {
     svenska: [
       "Min resa inom programmering började redan 2019 när jag tog min första onlinekurs i C#. Framåt julen samma år blev jag klar med kursen och började ta andra onlinekurser som riktade in sig på spelprogrammering.",
+      "2022 började jag en utbildning på KYH inom frontend och har tagit examen till slutet på våren 2024. Även om utbildningen var inriktad på frontend så fick jag med mig mycket kunskap om backend också med NodeJS och MySQL.",
+      "Under min utbildning har jag hjälpt många av mina klasskamrater med olika problem de fått och har i 95% av fallen lyckats komma fram till en lösning.",
+      "Trots att jag inte haft ett jobb inom utveckling så har jag utvecklat system som används av andra människor. Till exempel så har jag byggt ett system för ett politiskt parti som de använder för att hantera vallokaler under val där man kan ange vilka utdelare som ska dela ut valsedlar till vallokalerna. Utdelarna får även en länk som de kan gå in på för att enkelt se vilka vallokaler man blivit tilldelad och kan uppdatera dens status om man delat valsedlar dit eller inte.",
     ],
     english: [
       "My journey in programming started in 2019 when I took my first online course in C#. I finished the course right before christmas the same year and started taking other online courses that focused on game development.",
+      "In 2022 I took classes for a frontend education at KYH and will graduate in 2024. Even though the classes are focused on frontend, I've learned a lot about backend as well with technologies such as NodeJS and MySQL.",
+      "During my education I've helped many of my classmates with problems they've had and 95% of the time I've managed to reach a solution.",
+      "Even though I've not had a job as a developer I've worked on projects that are used by other people. I built a system for a political party they use to manage polling stations during elections where they can assign people to cover the stations with ballots. The people who get assigned receive a personal link where they can easily access which polling stations they've been dealt and can update the status of the polling station wether it has been covered or not.",
     ],
   },
   miscInfo: {
